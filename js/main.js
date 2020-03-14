a2877ea26420d012f3b6770f9c46b21554b8a882
Irina <55109138+IrinaSKab@users.noreply.github.com>
2020-03-15 02:42:52 +0300

    feat: close modal window by esc and click on space

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
+//отслеживаем событие на сайте для выполнения функции
   const modal = document.querySelector('.modal');
+  //обращаемся к модальному окну
+
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+ //находим все элементы, которые имеют специальный атрибут data-toggle="modal"
+
   const closeBtn = document.querySelector('.modal__close');
+ //закрытие модального окна
+
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
+  //функция, которая включает или выключает модальное окно
+
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
+  //цикл, который перебирает все кнопки, которые попали в переменную "modalBtn"
 
   closeBtn.addEventListener('click', switchModal);
-  
+  //закрытие модального окна при нажатии на "крестик"
+
+  document.addEventListener('keydown', function(event) {
+    if (event.code == 'Escape') switchModal();
+  });
+  //закрытие модального окна при нажатии на клавишу Esc
+
+  document.addEventListener('click', function(event) {
+    if (event.target == modal) switchModal();
+  });
+  //закрытие модального окна кликом по пустому месту за его пределами
 
 });
+
