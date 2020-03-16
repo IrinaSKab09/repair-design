9565111c1f329c6d0d838f69f75afe55180d82f7
Irina <55109138+IrinaSKab@users.noreply.github.com>
2020-03-16 23:13:09 +0300

    feat: add section projects

--- js/main.js
+++ js/main.js
@@ -60,5 +60,24 @@ $(document).ready(function () {
     modal.toggleClass('modal--visible');
   });
 
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next')
+  var prev = $('.swiper-button-prev')
+  var bullets = $('.swiper-pagination')
+
+  next.css('left', prev.width() + bullets.width() + 42)
+  bullets.css('left', prev.width() + 21)
+
 });
 
